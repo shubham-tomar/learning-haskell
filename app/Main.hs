315812a9e5d33a7e29ac7afb4804eefab9804af9
shubham.tomar <shubham.tomar@juspay.in>
2024-10-01 07:54:14 +0530

    init

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@
 module Main where
 
 import Text.Printf
+-- import Data.Text (Text, pack)
 
 main :: IO ()
 main = printf "hi %.2f " $ compounding 10
@@ -11,3 +12,23 @@ main = printf "hi %.2f " $ compounding 10
 compounding :: Int  -> Double
 compounding 0 = 1000
 compounding n = 1.05 * compounding(n-1)
+
+add :: Int -> Int -> Int
+add x y = x + y
+
+add2 :: Int -> (Int -> Int)
+add2 = (\x -> (\y -> x + y))
+
+inc :: Int -> Int
+inc x = x + 1
+
+toStr :: Int -> String
+toStr x = show x
+
+comp :: (Int -> Int) -> (Int -> String) -> Int -> String
+comp x y num = y $ x num
+
+_foldr :: (Int -> Int -> Int) -> Int -> [Int] -> Int
+_foldr _ acc []     = acc
+_foldr f acc (x:xs) = f x (foldr f acc xs)
+
